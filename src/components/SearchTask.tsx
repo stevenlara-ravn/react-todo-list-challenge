f270d8fab7cbb1a616b7c3f432f70af66f5927f4
Steven Lara <stevencarcamo@ravn.co>
2024-12-23 15:39:29 -0600

    fix: Fix error when filtering data

--- src/components/SearchTask.tsx
+++ src/components/SearchTask.tsx
@@ -6,33 +6,38 @@ import { Task } from "../types/Task";
 export default function SearchTask() {
   const [searchTerm, setSearchTerm] = useState<string>("");
   const [termDebounced] = useDebounce(searchTerm, 500);
-  const tasks = useTasksStore((state: TasksStore) => state.tasks) as Task[];
+  const allTasks = useTasksStore((state: TasksStore) => state.allTasks);
   const updateTasks = useTasksStore((state: TasksStore) => state.updateTasks);
+  const resetTasks = useTasksStore((state: TasksStore) => state.resetTasks);
 
   const handleSearch = (e: React.FormEvent<HTMLInputElement>) => {
     setSearchTerm(e.currentTarget.value);
-  }
+  };
 
   useEffect(() => {
-    if (termDebounced.length > 0) {
-      const filteredTasks = tasks.filter((task: Task) => {
-        return task.title.toLowerCase().includes(termDebounced.toLowerCase())
-          || task.assignee.toLowerCase().includes(termDebounced.toLowerCase());
+    if (termDebounced.trim().length > 0) {
+      const filteredTasks = allTasks.filter((task: Task) => {
+        return (
+          task.title.toLowerCase().includes(termDebounced.toLowerCase()) ||
+          task.assignee.toLowerCase().includes(termDebounced.toLowerCase())
+        );
       });
 
       updateTasks(filteredTasks);
+    } else {
+      resetTasks();
     }
-
-  }, [termDebounced]);
+  }, [termDebounced, allTasks, updateTasks, resetTasks]);
 
   return (
-    <div
-      className="flex flex-col items-center justify-center w-full h-14"
-    >
+    <div className="flex flex-col items-center justify-center w-full h-14">
       <input
-        className="w-full h-14 px-5 nm-flat-white-sm rounded-xl" type="text" placeholder="Let's find something for today"
+        className="w-full h-14 px-5 nm-flat-white-sm rounded-xl"
+        type="text"
+        placeholder="Let's find something for today"
         onChange={handleSearch}
+        value={searchTerm}
       />
     </div>
-  )
+  );
 }
